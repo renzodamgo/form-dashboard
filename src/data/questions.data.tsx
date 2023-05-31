65db2e7f533b111f8b27cdc73e14e3dc47c19687
Renzo Damian <renzo.damian.go@gmail.com>
2023-05-31 03:19:43 -0500

    Add qeustions and styling

---
+++ src/data/questions.data.tsx
@@ -0,0 +1,112 @@
+export const questions = [
+  {
+    questionId: "tired",
+    question: "How tired are you feeling?",
+    left: "Not at all tired",
+    rigth: "Extremely tired",
+  },
+  {
+    questionId: "sleepy",
+    question: "Are you feeling at all sleepy?",
+    left: "Not at all sleepy",
+    rigth: "Extremely sleepy",
+  },
+  {
+    questionId: "drowsy",
+    question: "How drowsy do you feel?",
+    left: "Not at all drowsy",
+    rigth: "Extremely drowsy",
+  },
+  {
+    questionId: "fatigued",
+    question: "How fatigued would you say you are?",
+    left: "Not at all fatigued",
+    rigth: "Extremely fatigued",
+  },
+  {
+    questionId: "wornOut",
+    question: "Do you feel worn out right now?",
+    left: "Not at all worn out",
+    rigth: "Extremely worn out",
+  },
+  {
+    questionId: "energetic",
+    question: "How energetic would you say you currently are?",
+    left: "Not at all energetic",
+    rigth: "Extremely energetic",
+  },
+  {
+    questionId: "active",
+    question: "How active do you feel at the moment?",
+    left: "Not at all active",
+    rigth: "Extremely active",
+  },
+  {
+    questionId: "vigorous",
+    question: "Would you say you are feeling vigorous right now?",
+    left: "Not at all vigorous",
+    rigth: "Extremely vigorous",
+  },
+  {
+    questionId: "efficient",
+    question: "Do you feel efficient today?",
+    left: "Not at all efficient",
+    rigth: "Extremely efficient",
+  },
+  {
+    questionId: "lively",
+    question: "How lively are you right now?",
+    left: "Not at all lively",
+    rigth: "Extremely lively",
+  },
+  {
+    questionId: "bushed",
+    question: "Are you feeling bushed?",
+    left: "Not at all bushed",
+    rigth: "Totally bushed",
+  },
+  {
+    questionId: "exhausted",
+    question: "Do you feel exhausted right now?",
+    left: "Not at all exhausted",
+    rigth: "Totally exhausted",
+  },
+  {
+    questionId: "eyesOpen",
+    question: "Is keeping your eyes open easy or difficult right now?",
+    left: "Keeping my eyes open is no effort at all",
+    rigth: "Keeping my eyes open is a tremendous chore",
+  },
+  {
+    questionId: "moving",
+    question: "Is moving your body easy or difficult at the moment?",
+    left: "Moving my body is no effort at all",
+    rigth: "Moving my body is a tremendous chore",
+  },
+  {
+    questionId: "concentrate",
+    question: "Are you finding it easy to concentrate right now?",
+    left: "Concentrating is no effort at all",
+    rigth: "Concentrating is a tremendous chore",
+  },
+  {
+    questionId: "conversation",
+    question:
+      "How easy or difficult is it for you to carry on a conversation right now?",
+    left: "Carrying on a conversation is no effort at all",
+    rigth: "Carrying on a conversation is a tremendous chore",
+  },
+  {
+    questionId: "closeEyes",
+    question:
+      "Do you have any desire to close your eyes at the present moment?",
+    left: "I have absolutely no desire to close my eyes",
+    rigth: "I have a tremendous desire to close my eyes",
+  },
+  {
+    questionId: "lieDown",
+    question: "Are you feeling any desire to lie down right now?",
+    left: "I have absolutely no desire to lie down",
+    rigth: "I have a tremendous desire to lie down",
+  },
+];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,61 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import { ChangeEvent, useEffect, useState } from "react";
+import { questions } from "~/data/questions.data";
+
+type formTypes = {
+  name: string;
+  lastName: string;
+  answers: {
+    questionId: string;
+    answer: string;
+  }[];
+};
 
 const Home: NextPage = () => {
+  const [formData, setFormData] = useState<formTypes>({
+    name: "",
+    lastName: "",
+    answers: [],
+  });
+
+  const handleNames = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((formData) => ({ ...formData, [name]: value }));
+  };
+
+  const handleAnswer = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    const existingAnswerIndex = formData.answers.findIndex(
+      (answer) => answer.questionId === name
+    );
+    if (existingAnswerIndex != -1) {
+      const updatedAnswers = [...formData.answers];
+      updatedAnswers[existingAnswerIndex] = {
+        questionId: name,
+        answer: value,
+      };
+      setFormData((formData) => ({
+        ...formData,
+        answers: [...updatedAnswers],
+      }));
+      return;
+    }
+    setFormData((formData) => ({
+      ...formData,
+      answers: [
+        ...formData.answers,
+        {
+          questionId: name,
+          answer: value,
+        },
+      ],
+    }));
+  };
+  useEffect(() => {
+    console.log({ ...formData.answers });
+  }, [formData]);
+
   return (
     <>
       <Head>
@@ -9,13 +63,52 @@ const Home: NextPage = () => {
         <meta name="description" content="VAS-F Form to log user fatigue" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen bg-gradient-to-b from-[#411c72] to-[#070b65]">
-        <div className="container flex gap-12 px-4 py-16 ">
-          <h1 className="text-3xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+      <main className="flex min-h-screen bg-gradient-to-b from-[#411c72] to-[#070b65] text-white">
+        <div className="container mx-auto flex flex-col gap-2 px-4 py-12 ">
+          <h1 className=" pb-2 text-6xl font-extrabold  ">
+            DriveSafe Fatigue Form
+            <br />
+          </h1>
+          <h1 className=" pb-6 text-3xl font-extrabold  ">
             Visual Analogue Scale for Fatigue
             <br />
             <span className="text-[hsl(280,100%,70%)]">VAS-F</span>
           </h1>
+          <div className="grid gap-5">
+            {questions.map((question, index) => (
+              <div
+                key={index}
+                className={`rounded-lg bg-[hsl(0,0%,0%,0.33)] px-6 py-4 ring-2  ${
+                  formData.answers.find(
+                    (a) => question.questionId == a.questionId
+                  )
+                    ? "ring-green-500"
+                    : "ring-blue-500"
+                }`}
+              >
+                <h3 className="pb-4 text-2xl font-semibold tracking-tight">
+                  {question.question}
+                </h3>
+                <input
+                  type="range"
+                  min="0"
+                  max="100"
+                  value={
+                    formData.answers[index]?.answer
+                      ? formData.answers[index]?.answer
+                      : 50
+                  }
+                  className="w-full"
+                  name={question.questionId}
+                  onChange={handleAnswer}
+                />
+                <div className="flex justify-between text-sm font-semibold text-white">
+                  <p className="max-w-[180px]">{question.left}</p>
+                  <p className="max-w-[180px]">{question.rigth}</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </main>
     </>
