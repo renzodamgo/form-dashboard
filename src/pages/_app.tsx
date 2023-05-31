57d7c2b98fefb4ea8deb9673b89f127c8be59909
Renzo Damian <renzo.damian.go@gmail.com>
2023-05-31 00:51:01 -0500

    Remove auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,9 @@
-import { ClerkProvider } from "@clerk/nextjs";
 import { type AppType } from "next/app";
 import "~/styles/globals.css";
 import { api } from "~/utils/api";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return (
-    <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
-    </ClerkProvider>
-  );
+  return <Component {...pageProps} />;
 };
 
 export default api.withTRPC(MyApp);
